39d5b0a70c9c20a41f1e3b77bfd93396bb1fe2c5
Dmytro <bakay.dmytro@gmail.com>
2021-02-15 17:41:51 +0200

    add map modal

---
+++ src/components/Map/Categories.js
@@ -0,0 +1,38 @@
+import React, {useEffect, useState} from "react";
+import {Select} from "antd";
+import {mapSearchParameters} from "../../context/SearchContext";
+import {getClubsByParameters} from "../../service/ClubService";
+import {getAllCategories} from "../../service/CategoryService";
+import './css/Categories.css';
+
+
+const Categories = ({setMapClubs}) => {
+    const { Option } = Select;
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        getAllCategories().then(response => setCategories(response));
+    }, []);
+
+    const onCityChange = (value) => {
+        mapSearchParameters.categoryName = value;
+        getClubsByParameters(mapSearchParameters).then(response => setMapClubs(response)
+            );
+    }
+
+    return (
+        <Select
+            className="selectCity"
+            onChange={onCityChange}
+            showSearch
+            placeholder="Всі категорії"
+            style={{borderRadius:'15px'}}
+        >
+            <Option value="">всі категорії</Option>
+            {categories.map(category => (<Option value={category.name} >{category.name}</Option>))}
+
+        </Select>
+    )
+}
+
+export default Categories;
---
+++ src/components/Map/ClubItem.js
@@ -0,0 +1,31 @@
+import EnvironmentFilled from "@ant-design/icons/lib/icons/EnvironmentFilled";
+import React from "react";
+import './css/ClubItem.css'
+import {MapSelectContext, MapZoomContext} from "../../context/SearchContext";
+import {useContext} from "react";
+
+const ClubItem = ({mapClub}) => {
+    const{selected, setSelected} = useContext(MapSelectContext);
+    const{zoom, setZoom} = useContext(MapZoomContext);
+
+    return (
+        <div onClick={() =>{
+            setSelected(mapClub)
+            setZoom(15)
+        }} className="clubItem">
+            <div className="title">
+                <div className="icon-box" style={{backgroundColor: mapClub.categories[0].backgroundColor}} >
+                    <img className="icon" src={mapClub.categories[0].urlLogo} alt="Category logo"/>
+                </div>
+                <div className="name">
+                    {mapClub.name}
+                </div>
+            </div>
+            <div className="content">
+                <div className="address"> <EnvironmentFilled className="address-icon"/>{mapClub.address}</div>
+            </div>
+        </div>
+    )
+};
+
+export default ClubItem;
