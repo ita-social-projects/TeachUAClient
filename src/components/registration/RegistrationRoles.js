9508027de8b452ae6c528db8f862344fe6f9dfb2
Denis Burko <ealightpawn@gmail.com>
2021-02-24 14:08:15 +0200

    fixed reg page

--- src/components/registration/RegistrationRoles.js
+++ src/components/registration/RegistrationRoles.js
@@ -1,34 +1,32 @@
 import React from 'react';
 import UserOutlined from "@ant-design/icons/lib/icons/UserOutlined";
+import {Form} from "antd";
 import Radio from "antd/es/radio/radio";
 
 const RegistrationRoles = ({setDisabledButton}) => {
-
-    const onChangeHandle = (e) => {
-        setDisabledButton(false)
-        console.log(e.target.value)
-    }
-
     return (
-        <Radio.Group className="button-container"
-                     options={[{
-                         label: (
-                             <div className="button-box">
-                                 <div className="ellipse"><UserOutlined className="user-icon"/></div>
-                                 Відвідувач
-                             </div>),
-                         value: 'user'
-                     }, {
-                         label: (
-                             <div className="button-box">
-                                 <div className="ellipse"><UserOutlined className="user-icon"/></div>
-                                 Керівник
-                             </div>), value: 'owner'
-                     }]}
-                     onChange={onChangeHandle}
-                     optionType="button"
-                     buttonStyle="solid"
-        />
+        <Form.Item name="role">
+            <Radio.Group className="button-container"
+                         options={[{
+                             label: (
+                                 <div className="button-box">
+                                     <div className="ellipse"><UserOutlined className="user-icon"/></div>
+                                     Відвідувач
+                                 </div>),
+                             value: 'ROLE_USER'
+                         }, {
+                             label: (
+                                 <div className="button-box">
+                                     <div className="ellipse"><UserOutlined className="user-icon"/></div>
+                                     Керівник
+                                 </div>),
+                             value: 'ROLE_ADMIN'
+                         }]}
+                         onChange={() => setDisabledButton(false)}
+                         optionType="button"
+                         buttonStyle="solid"
+            />
+        </Form.Item>
     )
 }
 
