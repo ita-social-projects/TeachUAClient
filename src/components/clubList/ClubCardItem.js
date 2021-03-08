4dc205b30b2935bae7da1cf74cd8a8fbe14c3ecc
Dmytro <bakay.dmytro@gmail.com>
2021-03-09 00:17:33 +0200

    add club info modal component

--- src/components/clubList/ClubCardItem.js
+++ src/components/clubList/ClubCardItem.js
@@ -1,25 +1,33 @@
-import {Button, Card, Rate} from "antd";
+import { Button, Card, Rate } from "antd";
 import EnvironmentFilled from "@ant-design/icons/lib/icons/EnvironmentFilled";
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import Tags from "../Tags";
 import CategoryLogo from "../CategoryLogo";
-import {convertFromJson, convertToHtml, getShortContent} from "../editor/EditorConverter";
+import { getShortContent } from "../editor/EditorConverter";
+import ClubInfo from "../clubInfo/ClubInfo";
 
 
-const ClubCardItem = ({club}) => {
+const ClubCardItem = ({ club }) => {
+
+    const [visible, setVisible] = useState(false);
+
+    const onClick = () => {
+        setVisible(true);
+    }
+
     return (
-        <Card className="card">
+        <Card className="card" onClick={() => onClick()}>
             <div className="title">
-                <CategoryLogo category={club.categories[0]}/>
+                <CategoryLogo category={club.categories[0]} />
                 <div className="name">{club.name}</div>
             </div>
-            <Tags categories={club.categories}/>
+            <Tags categories={club.categories} />
             {club.center !== null ?
                 <div className="with-center">
                     <div className="center">
-                        <img className="center-logo" src={club.center.urlLogo} alt="Center logo"/>
+                        <img className="center-logo" src={club.center.urlLogo} alt="Center logo" />
                         <div className="center-description">
                             <span className="center-label">Центр розвитку</span>
                             <span className="center-name">{club.center.name}</span>
@@ -31,12 +39,14 @@ const ClubCardItem = ({club}) => {
             <Rate className="rating" disabled value={club.rating} />
             <div className="address">
                 <EnvironmentFilled
-                    className="address-icon"/>
+                    className="address-icon" />
                 <span className="text"> {club.address}</span>
             </div>
             <Button className="outlined-button details-button">
                 <Link to={`/club/${club.id}`}>Детальніше</Link>
             </Button>
+
+            <ClubInfo visible={visible} setVisible={setVisible} club={club} />
         </Card>
     )
 };
