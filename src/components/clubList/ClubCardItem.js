b58f77e95db6e791e6af2cec29c36eac11d4fd2d
Dmytro <bakay.dmytro@gmail.com>
2021-03-09 11:49:10 +0200

    modify club card item

--- src/components/clubList/ClubCardItem.js
+++ src/components/clubList/ClubCardItem.js
@@ -18,36 +18,37 @@ const ClubCardItem = ({ club }) => {
     }
 
     return (
-        <Card className="card" onClick={() => onClick()}>
-            <div className="title">
-                <CategoryLogo category={club.categories[0]} />
-                <div className="name">{club.name}</div>
-            </div>
-            <Tags categories={club.categories} />
-            {club.center !== null ?
-                <div className="with-center">
-                    <div className="center">
-                        <img className="center-logo" src={club.center.urlLogo} alt="Center logo" />
-                        <div className="center-description">
-                            <span className="center-label">Центр розвитку</span>
-                            <span className="center-name">{club.center.name}</span>
+        <div>
+            <Card className="card" onClick={() => onClick()}>
+                <div className="title">
+                    <CategoryLogo category={club.categories[0]} />
+                    <div className="name">{club.name}</div>
+                </div>
+                <Tags categories={club.categories} />
+                {club.center !== null ?
+                    <div className="with-center">
+                        <div className="center">
+                            <img className="center-logo" src={club.center.urlLogo} alt="Center logo" />
+                            <div className="center-description">
+                                <span className="center-label">Центр розвитку</span>
+                                <span className="center-name">{club.center.name}</span>
+                            </div>
                         </div>
-                    </div>
-                    <div className="description">{getShortContent(club.description)}</div>
-                </div> :
-                <p className="description">{getShortContent(club.description)}</p>}
-            <Rate className="rating" disabled value={club.rating} />
-            <div className="address">
-                <EnvironmentFilled
-                    className="address-icon" />
-                <span className="text"> {club.address}</span>
-            </div>
-            <Button className="outlined-button details-button">
-                <Link to={`/club/${club.id}`}>Детальніше</Link>
-            </Button>
-
+                        <div className="description">{getShortContent(club.description)}</div>
+                    </div> :
+                    <p className="description">{getShortContent(club.description)}</p>}
+                <Rate className="rating" disabled value={club.rating} />
+                <div className="address">
+                    <EnvironmentFilled
+                        className="address-icon" />
+                    <span className="text"> {club.address}</span>
+                </div>
+                <Button className="outlined-button details-button">
+                    <Link to={`/club/${club.id}`}>Детальніше</Link>
+                </Button>
+            </Card>
             <ClubInfo visible={visible} setVisible={setVisible} club={club} />
-        </Card>
+        </div>
     )
 };
 ClubCardItem.propTypes = {
