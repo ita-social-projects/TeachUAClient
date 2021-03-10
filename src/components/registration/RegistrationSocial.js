a7683c4c75d8a4c1f23cd7256bdd2b774e4dcbb4
bakaydmytro <bakay.dmytro@gmail.com>
2021-03-10 23:10:01 +0200

    change css style for registration form

--- src/components/registration/RegistrationSocial.js
+++ src/components/registration/RegistrationSocial.js
@@ -1,12 +1,19 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GOOGLE_AUTH_URL, FACEBOOK_AUTH_URL } from '../../service/config/AuthConfig';
 
 
 const RegistrationSocial = ({ role }) => {
+    const [style, setStyle] = useState("items");
+    useEffect(() => {
+        if (role) {
+            setStyle("items-active")
+        }
+    }, [role])
+
 
     return (
         <div className="icons">
-            <div className="items">
+            <div className={style}>
                 <a target="_blank" href={GOOGLE_AUTH_URL + "&role=" + role}> <img src={`${process.env.PUBLIC_URL}/static/images/user/google.png`} className="logo" alt="Logo" />
                 </a>
                 <a target="_blank" href={FACEBOOK_AUTH_URL + "&role=" + role}> <img src={`${process.env.PUBLIC_URL}/static/images/user/facebook.png`} className="logo" alt="Logo" />
