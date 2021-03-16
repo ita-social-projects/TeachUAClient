497117b666f1a3f3e4f47840bb9191a9e6bd6dfb
bakaydmytro <bakay.dmytro@gmail.com>
2021-03-16 15:20:08 +0200

    add About Project page

--- src/components/userPage/content/UserCenterList.js
+++ src/components/userPage/content/UserCenterList.js
@@ -1,13 +1,12 @@
-import React, {useEffect, useState} from "react";
-import {withRouter} from "react-router";
-import {getCentersByUserId} from "../../../service/CenterService";
+import React, { useEffect, useState } from "react";
+import { withRouter } from "react-router";
+import { getCentersByUserId } from "../../../service/CenterService";
 import Loader from "../../Loader";
-import EmptySearch from "../../clubList/EmptySearch";
-import {Layout, Pagination, Space} from "antd";
+import { Layout, Pagination, Space } from "antd";
 import UserCenterCardItem from "./UserCenterCardItem";
 import './css/UserCenter.less';
 
-const UserCenterList = ({load, setLoad, match}) => {
+const UserCenterList = ({ load, setLoad, match }) => {
     const [centers, setCenters] = useState({
         content: [],
         pageable: {},
@@ -30,8 +29,8 @@ const UserCenterList = ({load, setLoad, match}) => {
     };
 
     useEffect(() => {
-            getData()
-        }, []
+        getData()
+    }, []
     )
 
     const onPageChange = (page) => {
@@ -40,18 +39,18 @@ const UserCenterList = ({load, setLoad, match}) => {
     };
 
 
-    return load ? <Loader/> : centers.content.length === 0 ? <EmptySearch/> : (
+    return load ? <Loader /> : (
         <Layout className="user-centers">
             <Space wrap className="centers-cards" size={[0, 0]}>
-                {centers.content.map((center, index) => <UserCenterCardItem center={center} key={index}/>)}
+                {centers.content.map((center, index) => <UserCenterCardItem center={center} key={index} />)}
             </Space>
             <Pagination className="user-clubs-pagination"
-                        hideOnSinglePage
-                        showSizeChanger={false}
-                        onChange={onPageChange}
-                        current={page + 1}
-                        pageSize={centers.size}
-                        total={centers.totalElements}/>
+                hideOnSinglePage
+                showSizeChanger={false}
+                onChange={onPageChange}
+                current={page + 1}
+                pageSize={centers.size}
+                total={centers.totalElements} />
         </Layout>
     )
 }
