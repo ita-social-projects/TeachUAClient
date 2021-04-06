7d553f0210a82532dac85866550e4d30b197d488
vitalik1240 <vitalik1240@gmail.com>
2021-04-06 19:35:17 +0300

    Added ui button for db export

--- src/components/header/AuthMenu.js
+++ src/components/header/AuthMenu.js
@@ -8,6 +8,7 @@ import {Link} from "react-router-dom";
 import AddClubModal from "../addClub/AddClubModal";
 import Login from "../login/Login";
 import {deleteToken, deleteUserId, getToken} from "../../service/StorageService";
+import {DOWNLOAD_DATABASE_SQL} from "../../service/config/ApiConfig";
 
 const {SubMenu} = Menu;
 
@@ -34,6 +35,7 @@ const AuthMenu = () => {
                         <Menu.Item><Link to="/admin/users">Користувачі</Link></Menu.Item>
                         <Menu.Item><Link to="/admin/questions">FAQ</Link></Menu.Item>
                         <Menu.Item><Link to="/admin/import-database">Імпортувати дані</Link></Menu.Item>
+                        <Menu.Item><Link target="_blank" to={{pathname: DOWNLOAD_DATABASE_SQL}} download>Експортувати дані</Link></Menu.Item>
                     </SubMenu>
                 </Menu>
             )
