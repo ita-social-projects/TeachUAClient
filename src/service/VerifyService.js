68c528a5b56c1bc9cd03a2584d947b1f4740aa5a
jekaBas <jekisbas@gmail.com>
2021-04-29 10:36:02 +0300

    fix verify user by email

---
+++ src/service/VerifyService.js
@@ -0,0 +1,11 @@
+import fetchRequest from "./FetchRequest";
+import {BASE_URL} from "./config/ApiConfig";
+
+export const verifyUser = async (verifyCode) => {
+    return await fetchRequest.get(BASE_URL + "/api/verify?code=" + verifyCode)
+        .then((response) => {
+            return response;
+        }).catch((error) => {
+            return error.response;
+        });
+};
