bb62960c601059b3ad61fa0da1ef5b5dbde56a19
Vitalii <kindratvit24@gmail.com>
2021-05-11 18:14:18 +0300

    Fix slider pop-up on advanced search button click

--- src/components/Search.js
+++ src/components/Search.js
@@ -134,7 +134,11 @@ class Search extends React.Component {
 
     handleAdvancedSearch = () => {
         if (this.props.redirect) {
-            this.props.history.push("/clubs");
+            //redirect with props value for component
+            this.props.history.push({
+                pathname: "/clubs",
+                state: {showAdvancedSearch: true}
+            });
         }
         else {
             if (this.props.advancedSearch) {
