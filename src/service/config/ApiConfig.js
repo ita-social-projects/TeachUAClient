ead55f736517917000007c6ff95e50409da460dd
olesKukhto <68856835+olesKukhto@users.noreply.github.com>
2021-05-12 20:34:41 +0300

    Revert "Correct validation"

--- src/service/config/ApiConfig.js
+++
@@ -1,4 +0,0 @@
-export const BASE_URL = process.env.REACT_APP_ROOT_SERVER + process.env.PUBLIC_URL;
-export const UPLOAD_IMAGE_URL = BASE_URL + "/api/upload-image";
-export const UPLOAD_EXCEL = BASE_URL + "/api/upload-excel";
-export const DOWNLOAD_DATABASE_SQL = BASE_URL + "/api/download-database-sql";
--- src/service/config/AuthConfig.js
+++
@@ -1,7 +0,0 @@
-import { BASE_URL } from "./ApiConfig"
-
-export const OAUTH2_REDIRECT_URI = BASE_URL + 'oauth2/redirect';
-
-export const GOOGLE_AUTH_URL = BASE_URL + '/oauth2/authorize/google?redirect_uri=' + OAUTH2_REDIRECT_URI;
-
-export const FACEBOOK_AUTH_URL = BASE_URL + '/oauth2/authorize/facebook?redirect_uri=' + OAUTH2_REDIRECT_URI;
