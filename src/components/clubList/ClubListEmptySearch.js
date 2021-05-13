a7f2eed06f2c6cca0fdd7d5e916180518d2ccf9e
olesKukhto <olesKukhto@gmail.com>
2021-05-13 13:16:08 +0300

    spelling mistake

--- src/components/clubList/ClubListEmptySearch.js
+++ src/components/clubList/ClubListEmptySearch.js
@@ -6,7 +6,7 @@ const ClubListEmptySearch = () => {
     return (
         <div className="empty-advanced-search">
             <div className="clubs-not-found">За критеріями пошуку гуртків не знайдено</div>
-            <div className="invite-clubs">Заохочуйте гуртки до викладання укрїанською мовою та доєднання до платформи.</div>
+            <div className="invite-clubs">Заохочуйте гуртки до викладання українською мовою та доєднання до платформи.</div>
             <Button className="flooded-button invite-clubs-button">Заохотити гуртки</Button>
         </div>
     );
