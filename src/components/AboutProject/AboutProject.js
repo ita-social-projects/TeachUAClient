3200aa2e5d79093c10725854ca464b7ee92f9a8f
andriysavinof <andriy.savinof@gmail.com>
2021-05-17 18:33:00 +0300

    added resizeListener and made cleanup

--- src/components/AboutProject/AboutProject.js
+++ src/components/AboutProject/AboutProject.js
@@ -4,7 +4,7 @@ import "./css/aboutProject.css";
 import FacebookOutlined from "@ant-design/icons/lib/icons/FacebookOutlined";
 import YoutubeOutlined from "@ant-design/icons/lib/icons/YoutubeOutlined";
 import InstagramOutlined from "@ant-design/icons/lib/icons/InstagramOutlined";
-import {Button} from "antd";
+import { Button } from "antd";
 import ClubCarousel from "./ClubCarousel";
 import { getClubsByCategoryAndCity } from "../../service/ClubService";
 import { mapSearchParameters } from "../../context/SearchContext";
@@ -13,65 +13,99 @@ import Search from "../Search";
 const AboutProject = () => {
     const [clubs, setClubs] = useState([]);
     useEffect(() => {
-        getClubsByCategoryAndCity(mapSearchParameters).then(responce => {
+        getClubsByCategoryAndCity(mapSearchParameters).then((responce) => {
             setClubs(responce);
-        })
-    }, [mapSearchParameters.cityName])
+        });
+    }, []);
 
     return (
         <Layout className="aboutProject global-padding">
             <div className="lower-header-box about-header">
                 <div className="city-name-box">
-                    <h2 className="city-name">Ініціатива “Навчай українською”</h2>
+                    <h2 className="city-name">
+                        Ініціатива “Навчай українською”
+                    </h2>
                 </div>
-                <Search redirect/>
+                <Search redirect />
             </div>
-            <div className="title"
-                style={{ background: `url(${process.env.PUBLIC_URL}/static/images/service/about_project.png) center / cover` }}>
+            <div
+                className="title"
+                style={{
+                    background: `url(${process.env.PUBLIC_URL}/static/images/service/about_project.png) center / cover`,
+                }}>
                 <span className="text">Навчай українською</span>
                 <span className="content">Ініціатива</span>
             </div>
-            <p/><p/>
+            <p />
+            <p />
             <div className="social-info">
                 <div className="social-media">
                     <span className="text">Ми у соц. мережах</span>
                     <div className="links">
-                        <a target="_blank" href=""></a>
-                        <a target="_blank" href="https://www.facebook.com/teach.in.ukrainian"><FacebookOutlined className="icon"/></a>
-                        <a target="_blank" href="https://www.youtube.com/channel/UCP38C0jxC8aNbW34eBoQKJw"><YoutubeOutlined className="icon"/></a>
-                        <a target="_blank" href="https://www.instagram.com/teach.in.ukrainian/"><InstagramOutlined className="icon"/></a>
+                        {/* <a target="_blank" href="!#"></a> */}
+                        <a
+                            href="https://www.facebook.com/teach.in.ukrainian"
+                            target="_blank"
+                            rel="noreferrer">
+                            <FacebookOutlined className="icon" />
+                        </a>
+                        <a
+                            target="_blank"
+                            href="https://www.youtube.com/channel/UCP38C0jxC8aNbW34eBoQKJw"
+                            rel="noreferrer">
+                            <YoutubeOutlined className="icon" />
+                        </a>
+                        <a
+                            target="_blank"
+                            rel="noreferrer"
+                            href="https://www.instagram.com/teach.in.ukrainian/">
+                            <InstagramOutlined className="icon" />
+                        </a>
                     </div>
                 </div>
                 <div className="help-button">
-                    <a target="blank" href="#"><Button className="flooded-button donate-button">Допомогти
-                        проєкту</Button> </a>
+                    <a target="blank" href="!#">
+                        <Button className="flooded-button donate-button">
+                            Допомогти проєкту
+                        </Button>{" "}
+                    </a>
                 </div>
             </div>
             <div className="content">
                 <div className="about-info">
-                    <div className="title-content">
-                        Про ініціативу
-                    </div>
+                    <div className="title-content">Про ініціативу</div>
                     <div className="content-text">
-                        Ініціатива “Навчай українською” - це небайдужі громадяни, які об'єдналися, щоб популяризувати українську мову у сфері освіти.
+                        Ініціатива “Навчай українською” - це небайдужі
+                        громадяни, які об'єдналися, щоб популяризувати
+                        українську мову у сфері освіти.
                     </div>
                 </div>
-                <section className="organaizer" >
+                <section className="organaizer">
                     <div className="row">
                         <div className="col">
-                            <img src={`${process.env.PUBLIC_URL}/static/images/service/profile_2.jpg`}></img>
+                            <img
+                                src={`${process.env.PUBLIC_URL}/static/images/service/profile_2.jpg`}
+                                alt=""></img>
                         </div>
                         <div className="col info">
                             <div className="text">
-                                <span >
-                                    Що зміниться, якщо в Україні всі розмовлятимуть українською? Чи змінить це ставлення громадян до своєї країни? Чи стане молодь більше поважати свій край? щось про те, чому вирішила заснувати ініціативу і кілька слів про себе)
-                            </span>
+                                <span>
+                                    Що зміниться, якщо в Україні всі
+                                    розмовлятимуть українською? Чи змінить це
+                                    ставлення громадян до своєї країни? Чи стане
+                                    молодь більше поважати свій край? щось про
+                                    те, чому вирішила заснувати ініціативу і
+                                    кілька слів про себе)
+                                </span>
                             </div>
                             <div className="name">
                                 <span>Наталка Федечко</span>
                             </div>
                             <div className="position">
-                                <span >Співорганізаторка Ініціативи “Навчай українською”</span>
+                                <span>
+                                    Співорганізаторка Ініціативи “Навчай
+                                    українською”
+                                </span>
                             </div>
                         </div>
                     </div>
@@ -79,58 +113,105 @@ const AboutProject = () => {
                         <div className="col info">
                             <div className="text">
                                 <span>
-                                    Витяг. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porta lorem mollis aliquam ut porttitor leo a diam sollicitudin. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Amet aliquam id diam maecenas.
+                                    Витяг. Lorem ipsum dolor sit amet,
+                                    consectetur adipiscing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna
+                                    aliqua. Porta lorem mollis aliquam ut
+                                    porttitor leo a diam sollicitudin. Ut
+                                    venenatis tellus in metus vulputate eu
+                                    scelerisque felis imperdiet. Amet aliquam id
+                                    diam maecenas.
                                 </span>
                             </div>
                             <div className="name">
-                                <span >Іванна Кобєлєва</span>
+                                <span>Іванна Кобєлєва</span>
                             </div>
                             <div className="position">
                                 <span>Засновниця проєкту</span>
                             </div>
                         </div>
                         <div className="col">
-                            <img src={`${process.env.PUBLIC_URL}/static/images/service/profile_1.jpg`}></img>
+                            <img
+                                src={`${process.env.PUBLIC_URL}/static/images/service/profile_1.jpg`}
+                                alt=""></img>
                         </div>
                     </div>
                 </section>
                 <section className="sport-clubs">
                     <div className="title-content sport">
-                        Ініціатива "Навчай українською" закликає викладачів спортивних секцій, тренерів навчати дітей українською мовою.
+                        Ініціатива "Навчай українською" закликає викладачів
+                        спортивних секцій, тренерів навчати дітей українською
+                        мовою.
                     </div>
                     <div className="content-text sport desc">
-                        Із 16 січня 2021 набуває чинності стаття 30 закону “Про забезпечення функціонування української мови як державної” про державну мову у сфері обслуговування споживачів. З 16 січня всі надавачі послуг, незалежно від форми власності, зобов’язані обслуговувати споживачів і надавати інформацію про товари і послуги державною мовою. Громадяни мають право отримати освітній послуги українською мовою у закладах позашкільної освіти.
+                        Із 16 січня 2021 набуває чинності стаття 30 закону “Про
+                        забезпечення функціонування української мови як
+                        державної” про державну мову у сфері обслуговування
+                        споживачів. З 16 січня всі надавачі послуг, незалежно
+                        від форми власності, зобов’язані обслуговувати
+                        споживачів і надавати інформацію про товари і послуги
+                        державною мовою. Громадяни мають право отримати освітній
+                        послуги українською мовою у закладах позашкільної
+                        освіти.
                     </div>
                     <div className="video">
-                        <iframe width="100%" height="378" style={{ borderRadius: '16px' }} src="https://www.youtube.com/embed/i3umBFqDznQ" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <iframe
+                            title="unique"
+                            width="100%"
+                            height="378"
+                            style={{ borderRadius: "16px" }}
+                            src="https://www.youtube.com/embed/i3umBFqDznQ"
+                            frameborder="0"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowfullscreen></iframe>
                     </div>
                 </section>
                 <ClubCarousel clubs={clubs} />
                 <section className="addit-information">
                     <div className="title-content ">
-                        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum
+                        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+                        lorem ipsum
                     </div>
                     <div className="content-text">
-                        Витяг. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porta lorem mollis aliquam ut porttitor leo a diam sollicitudin. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Amet aliquam id diam maecenas.
+                        Витяг. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Porta lorem mollis
+                        aliquam ut porttitor leo a diam sollicitudin. Ut
+                        venenatis tellus in metus vulputate eu scelerisque felis
+                        imperdiet. Amet aliquam id diam maecenas.
                     </div>
                     <div class="image">
-                        <img src={`${process.env.PUBLIC_URL}/static/images/service/about_img.jpg`}></img>
+                        <img
+                            src={`${process.env.PUBLIC_URL}/static/images/service/about_img.jpg`}
+                            alt=""></img>
                     </div>
                     <div className="title-content ">
-                        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum
+                        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+                        lorem ipsum
                     </div>
                     <div className="content-text">
-                        Витяг. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porta lorem mollis aliquam ut porttitor leo a diam sollicitudin. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Amet aliquam id diam maecenas.
+                        Витяг. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Porta lorem mollis
+                        aliquam ut porttitor leo a diam sollicitudin. Ut
+                        venenatis tellus in metus vulputate eu scelerisque felis
+                        imperdiet. Amet aliquam id diam maecenas.
                     </div>
                     <div className="title-content ">
-                        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet lorem ipsum
+                        Lorem ipsum dolor sit amet lorem ipsum dolor sit amet
+                        lorem ipsum
                     </div>
                     <div className="content-text">
-                        Витяг. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Porta lorem mollis aliquam ut porttitor leo a diam sollicitudin. Ut venenatis tellus in metus vulputate eu scelerisque felis imperdiet. Amet aliquam id diam maecenas.
+                        Витяг. Lorem ipsum dolor sit amet, consectetur
+                        adipiscing elit, sed do eiusmod tempor incididunt ut
+                        labore et dolore magna aliqua. Porta lorem mollis
+                        aliquam ut porttitor leo a diam sollicitudin. Ut
+                        venenatis tellus in metus vulputate eu scelerisque felis
+                        imperdiet. Amet aliquam id diam maecenas.
                     </div>
                 </section>
             </div>
-        </Layout >
-    )
-}
+        </Layout>
+    );
+};
 export default AboutProject;
--- src/components/mainPage/MainCategories.js
+++ src/components/mainPage/MainCategories.js
@@ -1,25 +1,28 @@
-import './css/AboutDescription.css';
+import "./css/AboutDescription.css";
 import React, { useState, useEffect } from "react";
 import { getPageableCategory } from "../../service/CategoryService";
 import PrimitiveCard from "../PrimitiveCard";
-import './css/AboutCategories.css';
+import "./css/AboutCategories.css";
 import CategoryLogo from "../CategoryLogo";
-import {Button, Pagination} from "antd";
-import './css/AboutCarousel.css';
+import { Button, Pagination } from "antd";
+import "./css/AboutCarousel.css";
 import ArrowLeftOutlined from "@ant-design/icons/lib/icons/ArrowLeftOutlined";
 import ArrowRightOutlined from "@ant-design/icons/lib/icons/ArrowRightOutlined";
 
 const MainCategories = () => {
     const [categories, setCategories] = useState({
         content: [],
-        size: 0
     });
+    const [pageSize, setPageSize] = useState(0);
     const [currentPage, setCurrentPage] = useState(0);
     const [totalPages, setTotalPages] = useState(0);
 
     const getData = (page) => {
-        getPageableCategory(page).then(response => {
+        getPageableCategory(page).then((response) => {
+            console.log("getData", pageSize, totalPages);
+            console.log(response);
             setCategories(response);
+            setPageSize(pageSize === 0 ? response.size : pageSize);
             setTotalPages(response.totalPages);
         });
     };
@@ -29,11 +32,13 @@ const MainCategories = () => {
     }, []);
 
     const onChange = (current) => {
+        console.log("onChange", pageSize);
         setCurrentPage(current - 1);
-        getData(current - 1)
-    }
+        getData(current - 1);
+    };
 
     const onPageChangePrev = (page) => {
+        onChange(page);
         if (page === 0) {
             setCurrentPage(totalPages - 1);
             getData(totalPages - 1);
@@ -44,6 +49,7 @@ const MainCategories = () => {
     };
 
     const onPageChangeNext = (page) => {
+        onChange(page);
         if (page === totalPages - 1) {
             setCurrentPage(0);
             getData(0);
@@ -51,20 +57,70 @@ const MainCategories = () => {
             setCurrentPage(page + 1);
             getData(page + 1);
         }
+    };
+
+    const getWidth = () =>
+        window.innerWidth ||
+        document.documentElement.clientWidth ||
+        document.body.clientWidth;
+
+    function useCurrentWidth() {
+        // save current window width in the state object
+        let [width, setWidth] = useState(getWidth());
+
+        // in this case useEffect will execute only once because
+        // it does not have any dependencies.
+        useEffect(() => {
+            // timeoutId for debounce mechanism
+            let timeoutId = null;
+            const resizeListener = () => {
+                // prevent execution of previous setTimeout
+                clearTimeout(timeoutId);
+                // change width from the state object after 150 milliseconds
+                timeoutId = setTimeout(() => {
+                    setWidth(getWidth());
+                    let itemsCount = Math.floor((getWidth() - 192) / 296);
+                    console.log(itemsCount);
+                    setPageSize(itemsCount);
+                }, 150);
+            };
+            // set resize listener
+            window.addEventListener("resize", resizeListener);
+
+            // clean up function
+            return () => {
+                // remove resize listener
+                window.removeEventListener("resize", resizeListener);
+            };
+        }, []);
+
+        return width;
     }
 
+    useCurrentWidth();
     return (
         <div className="about-categories">
             <div className="categories-header">
                 <h2 className="label">Оберіть напрям гуртків</h2>
-                <a href="/dev/clubs"><Button className="flooded-button more-button">Всі гуртки</Button> </a>
+                <a href="/dev/clubs">
+                    <Button className="flooded-button more-button">
+                        Всі гуртки
+                    </Button>{" "}
+                </a>
             </div>
             <div className="categories-carousel-block">
-                <ArrowLeftOutlined className="arrows-prev" onClick={() => onPageChangePrev(currentPage)} />
-                <ArrowRightOutlined className="arrows-next" onClick={() => onPageChangeNext(currentPage)} />
+                <ArrowLeftOutlined
+                    className="arrows-prev"
+                    onClick={() => onPageChangePrev(currentPage)}
+                />
+                <ArrowRightOutlined
+                    className="arrows-next"
+                    onClick={() => onPageChangeNext(currentPage)}
+                />
                 <div className="categories-cards">
-                    {categories.content.map(category =>
-                        <PrimitiveCard key={category.id}
+                    {categories.content.map((category) => (
+                        <PrimitiveCard
+                            key={category.id}
                             header={
                                 <div className="title">
                                     <CategoryLogo category={category} />
@@ -73,18 +129,20 @@ const MainCategories = () => {
                             }
                             description={category.description}
                             link={""}
-                            buttonText="Переглянути" />
-                    )}
+                            buttonText="Переглянути"
+                        />
+                    ))}
                 </div>
             </div>
             <Pagination
                 className="pagination"
+                responsive="true"
                 hideOnSinglePage
                 size="small"
                 showSizeChanger={false}
                 current={currentPage + 1}
-                pageSize={categories.size}
-                total={categories.totalElements}
+                pageSize={pageSize}
+                total={totalPages}
                 onChange={onChange}
             />
         </div>
