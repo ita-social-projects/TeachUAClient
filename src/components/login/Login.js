18cc42e2d415b04781f23d746350e926efe082e1
DashaFerbey <62422284+DariaFerbey@users.noreply.github.com>
2021-05-27 20:54:05 +0300

    fix add club button

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -22,7 +22,7 @@ const Login = ({isLogin}) => {
                 saveUserId(response.id);
                 saveToken(response.accessToken);
                 setLoginVisible(false);
-                isLogin("login");
+
             }
         });
     };
