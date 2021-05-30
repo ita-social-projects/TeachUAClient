97732a40275204a57f4382b32b51963a4202498d
bakaydmytro <bakay.dmytro@gmail.com>
2021-05-31 02:01:55 +0300

    fixed bugs related to map markers and add reload page after adding new club for updating list of users new club

--- src/components/addClub/steps/DescriptionStep.js
+++ src/components/addClub/steps/DescriptionStep.js
@@ -1,18 +1,18 @@
-import {Form, Input, Upload} from "antd";
-import React, {useRef, useEffect, useState} from "react";
+import { Form, Input, Upload } from "antd";
+import React, { useRef, useEffect, useState } from "react";
 import AddClubContentFooter from "../AddClubContentFooter";
 import UploadOutlined from "@ant-design/icons/lib/icons/UploadOutlined";
 import EditorComponent from "../../editor/EditorComponent";
-import {saveContent} from "../../editor/EditorConverter";
-import {transToEng} from "../../../util/Translit";
-import {UPLOAD_IMAGE_URL} from "../../../service/config/ApiConfig";
-import {addClub, getAllClubsByUserId} from "../../../service/ClubService";
+import { saveContent } from "../../editor/EditorConverter";
+import { transToEng } from "../../../util/Translit";
+import { UPLOAD_IMAGE_URL } from "../../../service/config/ApiConfig";
+import { addClub, getAllClubsByUserId } from "../../../service/ClubService";
 import "../css/AddClubContent.css";
-import {getUserId} from "../../../service/StorageService";
+import { getUserId } from "../../../service/StorageService";
 
-import {Button} from "antd";
+import { Button } from "antd";
 
-const DescriptionStep = ({step, setStep, setResult, result, setVisible, setLocations, clubs, setClubs}) => {
+const DescriptionStep = ({ step, setStep, setResult, result, setVisible, setLocations, clubs, setClubs }) => {
     const [descriptionForm] = Form.useForm();
     const clubName = transToEng(result.name.replace(/[^a-zA-ZА-Яа-яЁё0-9]/gi, ""));
 
@@ -22,7 +22,7 @@ const DescriptionStep = ({step, setStep, setResult, result, setVisible, setLocat
     useEffect(() => {
         if (result) {
             console.log(result);
-            descriptionForm.setFieldsValue({...result})
+            descriptionForm.setFieldsValue({ ...result })
         }
     }, []);
 
@@ -51,6 +51,7 @@ const DescriptionStep = ({step, setStep, setResult, result, setVisible, setLocat
                 })
             }
         });
+        window.location.reload();
     };
 
 
@@ -62,51 +63,51 @@ const DescriptionStep = ({step, setStep, setResult, result, setVisible, setLocat
             onFinish={onFinish}
             className="description-step">
             <Form.Item name="urlLogo"
-                       className="add-club-row"
-                       label="Логотип"
-                       hasFeedback>
+                className="add-club-row"
+                label="Логотип"
+                hasFeedback>
                 <Upload
                     name="image"
                     action={UPLOAD_IMAGE_URL}
                     maxCount={1}
-                    data={{folder: `clubs/${clubName}/logo`}}
-                    headers={{contentType: 'multipart/form-data'}}
+                    data={{ folder: `clubs/${clubName}/logo` }}
+                    headers={{ contentType: 'multipart/form-data' }}
                     showUploadList={false}
                 >
-                    <span className="add-club-upload"><UploadOutlined className="icon"/>Завантажити лого</span>
+                    <span className="add-club-upload"><UploadOutlined className="icon" />Завантажити лого</span>
                 </Upload>
             </Form.Item>
             <Form.Item name="urlBackground"
-                       className="add-club-row"
-                       label="Обкладинка"
-                       hasFeedback>
+                className="add-club-row"
+                label="Обкладинка"
+                hasFeedback>
                 <Upload
                     name="image"
                     action={UPLOAD_IMAGE_URL}
                     maxCount={1}
-                    data={{folder: `clubs/${clubName}/background`}}
-                    headers={{contentType: 'multipart/form-data'}}
+                    data={{ folder: `clubs/${clubName}/background` }}
+                    headers={{ contentType: 'multipart/form-data' }}
                     showUploadList={false}
                 >
-                    <span className="add-club-upload"><UploadOutlined className="icon"/>Завантажити обкладинку</span>
+                    <span className="add-club-upload"><UploadOutlined className="icon" />Завантажити обкладинку</span>
                 </Upload>
             </Form.Item>
             <Form.Item name="description"
-                       className="add-club-row"
-                       label="Опис"
-                       hasFeedback
-                       rules={[{
-                           required: true,
-                           pattern: /^(?!\s)([\wА-ЩЬЮЯҐЄІЇа-щьюяґєії @#$()%&{}, ,[\]^\/\\'’.,"!?:*|><]){40,1500}\S$|\s/
-                       }]}
-                >
-                <Input.TextArea className="editor-textarea" style={{height: 200}} placeholder="Додайте опис гуртка"/>
+                className="add-club-row"
+                label="Опис"
+                hasFeedback
+                rules={[{
+                    required: true,
+                    pattern: /^(?!\s)([\wА-ЩЬЮЯҐЄІЇа-щьюяґєії @#$()%&{}, ,[\]^\/\\'’.,"!?:*|><]){40,1500}\S$|\s/
+                }]}
+            >
+                <Input.TextArea className="editor-textarea" style={{ height: 200 }} placeholder="Додайте опис гуртка" />
             </Form.Item>
             <div className="add-club-content-footer">
                 <Button ghost={true} className="add-club-content-prev"
-                        type="button" onClick={prevStep}>Назад</Button>
+                    type="button" onClick={prevStep}>Назад</Button>
                 <Button className="flooded-button add-club-content-next"
-                        htmlType="submit">Завершити</Button>
+                    htmlType="submit">Завершити</Button>
             </div>
         </Form>
     )
