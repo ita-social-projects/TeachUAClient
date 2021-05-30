57dd5444a306e46aae33d0dd2fe501d44fe2006f
andriysavinof <andriy.savinof@gmail.com>
2021-05-30 22:11:06 +0300

    added onresize feature for categories carousel

--- src/components/PrimitiveCard.js
+++ src/components/PrimitiveCard.js
@@ -1,19 +1,25 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { forwardRef } from "react";
+import { Link } from "react-router-dom";
 import ArrowRightOutlined from "@ant-design/icons/lib/icons/ArrowRightOutlined";
-import './css/PrimitiveCard.css';
+import "./css/PrimitiveCard.css";
 import PropTypes from "prop-types";
 
-const PrimitiveCard = ({header, description, link, buttonText}) => {
+const PrimitiveCard = forwardRef((props, ref) => {
     return (
-        <div className="primitive-card">
+        <div className="primitive-card" ref={ref}>
             <div className="content">
-                {header}
-                <div className="description">{description}</div>
-                <Link to={link}><div className="details">{buttonText}<ArrowRightOutlined className="arrow"/></div></Link>
+                {props.header}
+                <div className="description">{props.description}</div>
+                <Link to={props.link}>
+                    <div className="details">
+                        {props.buttonText}
+                        <ArrowRightOutlined className="arrow" />
+                    </div>
+                </Link>
             </div>
-        </div>)
-};
+        </div>
+    );
+});
 /*PrimitiveCard.propTypes = {
     content: PropTypes.object.isRequired
 };*/
