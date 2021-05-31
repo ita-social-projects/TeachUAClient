a8396013e8858d264e03fa3eb8f41032a53ca2fb
marianben <marianben1995@gmail.com>
2021-05-31 13:18:54 +0300

    fix Wrong alignment for 'Центр' cards after search

--- src/components/centerList/CenterListItem.js
+++ src/components/centerList/CenterListItem.js
@@ -47,7 +47,7 @@ const CenterListItem = ({ center, onCenterClick }) => {
                                         </div>
                                     )}>
                                     <span className="text"><span className="oneAddress"
-                                    >{center.locations[0].address}</span>, і ще {center.locations.length - 1}</span>
+                                    >{center.locations[0].address}</span>{center.locations.length - 1}</span>
                                     <EyeOutlined className="expand-icon" />
                                 </Popover>
                         }
