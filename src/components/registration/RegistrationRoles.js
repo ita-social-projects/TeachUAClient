d50e9ee145f18898dcfadc2bea7deb87b945ecf0
DashaFerbey <62422284+DariaFerbey@users.noreply.github.com>
2021-07-09 14:02:02 +0300

    made the default field role

--- src/components/registration/RegistrationRoles.js
+++ src/components/registration/RegistrationRoles.js
@@ -2,18 +2,20 @@ import React from 'react';
 import UserOutlined from "@ant-design/icons/lib/icons/UserOutlined";
 import { Form, Radio } from "antd";
 
-const RegistrationRoles = ({ setDisabledButton, setRole }) => {
+const RegistrationRoles = ({setRole }) => {
     const onChange = e => {
         console.log(e.target.value);
-        setDisabledButton(false);
         setRole(e.target.value);
+        //setDisabledButton(false);
     }
+
     return (
-        <Form.Item name="role">
+        <Form.Item name="role" initialValue="ROLE_MANAGER" >
             <Radio.Group className="button-container"
-                onChange={onChange}
-                optionType="button"
-                buttonStyle="solid">
+                         onChange={onChange}
+                         defaultValue="ROLE_MANAGER"
+                         optionType="button"
+                         buttonStyle="solid">
                 {/*<Radio.Button value="ROLE_USER">*/}
                 {/*    <div className="button-box">*/}
                 {/*        <div className="ellipse"><UserOutlined className="user-icon" /></div>*/}
