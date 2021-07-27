8e9922e559fbbf75e346ebc12e728fe99adb7afd
DashaFerbey <62422284+DariaFerbey@users.noreply.github.com>
2021-07-27 19:05:10 +0300

    add club's addition pop-up for mobile version

--- src/components/addClub/AddClubSiderMobile.js
+++ src/components/addClub/AddClubSiderMobile.js
@@ -2,19 +2,26 @@ import {Checkbox, Form, Input, InputNumber, Steps} from "antd";
 import React from "react";
 import Sider from "antd/es/layout/Sider";
 import "./css/AddClubSider.css";
+import {onHidden} from "web-vitals/dist/lib/onHidden";
 
-const { Step } = Steps;
+const {Step} = Steps;
 
 const AddClubSiderMobile = ({step}) => {
+
     return (
-            <Steps className="add-club-sider-mobile"
-                   style={{background: "#FAFAFA"}}
-                   current={step}
-                   size={"small"} >
-                <Step title="Основна інформація" />
+        <Steps className="add-club-sider-mobile"
+               style={{background: "#FAFAFA"}}
+               current={step}>
+            {step == 0 &&
+                <Step title="Основна інформація"/>
+            }
+            {step == 1 &&
                 <Step title="Контакти"/>
+            }
+            {step == 2 &&
                 <Step title="Опис"/>
-            </Steps>
+            }
+        </Steps>
     )
 };
 
