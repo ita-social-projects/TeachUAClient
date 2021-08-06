b08a6accc0436fce6bfa945583ea6c45810b14bb
erdghst79 <61949771+erdghst79@users.noreply.github.com>
2021-08-06 17:37:44 +0300

    marathon button

--- src/components/marathonPage/MarathonRegistrationPage.js
+++ src/components/marathonPage/MarathonRegistrationPage.js
@@ -1,7 +1,8 @@
 import React, { useEffect } from "react";
 import { Layout, Menu } from "antd";
 import "./css/MarathonPage.css";
-
+import { Link } from "react-router-dom";
+import { Button } from "antd";
 import AboutHeader from "../mainPage/MainHeader";
 
 
@@ -16,7 +17,14 @@ const MarathonPage = () => {
             <AboutHeader />
           <div className = "google-form">
            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc44MDNWvgsw0caL1HLTyBV5dHckPkd0yb3t5HN4vwUe9ENxQ/viewform?embedded=true" width="700" height="2000" frameborder="0" marginheight="0" marginwidth="0">Завантаження…</iframe> 
+          
+      
+        
+           </div>
+         <div className = "google-form-button">
+         <Link to="/marathon"><Button className="marathon-button">Мовомаратон</Button></Link>
          </div>
+        
         </Layout>
     );
 };
