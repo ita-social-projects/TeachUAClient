01e1fd74bb448ed10f6231f9461c42d507ac9809
RostyslavNovoselskyi <rostuk2713@ukr.net>
2021-08-26 19:24:17 +0300

    fixed clicking on footer partner links opened in the same link

--- src/components/footer/FooterPartners.js
+++ src/components/footer/FooterPartners.js
@@ -7,11 +7,11 @@ const FooterPartners = () => {
                 Наші партнери
             </div>
             <div className="sponsors">
-            <a target="blank" href="https://www.softserveinc.com/uk-ua">
+                <a target="_blank" href="https://www.softserveinc.com/uk-ua">
                     <img src={`${process.env.PUBLIC_URL}/static/images/footer/sponsors/soft_serve_logo.png`}
                          className="logo_soft-serve" alt="soft_serve"/>
                 </a>
-                <a target="blank" href="https://www.facebook.com/zakonpromovu5670">
+                <a target="_blank" href="https://www.facebook.com/zakonpromovu5670">
                     <img src={`${process.env.PUBLIC_URL}/static/images/footer/sponsors/logo1.png`}
                          className="logo" alt="Mova_obyednue"/> </a>
 
@@ -20,7 +20,7 @@ const FooterPartners = () => {
                          className="logo" alt="EDERA"/>
                 </a>
 
-                <a target="blank" href="https://emova.language-ua.online">
+                <a target="_blank" href="https://emova.language-ua.online">
                     <img src={`${process.env.PUBLIC_URL}/static/images/footer/sponsors/logo3.png`}
                          className="logo" alt="e-mova"/>  </a>
 
@@ -28,17 +28,17 @@ const FooterPartners = () => {
                 {/*    <img src={`${process.env.PUBLIC_URL}/static/images/footer/sponsors/logo4.png`}*/}
                 {/*         className="logo" alt="Logo"/> </a>*/}
 
-                <a target="blank" href="https://krainafm.com.ua">
+                <a target="_blank" href="https://krainafm.com.ua">
                     <img src={`${process.env.PUBLIC_URL}/static/images/footer/sponsors/Kraina_FM.jpg`}
                          className="logo" alt="Kraina_FM"/>
                 </a>
 
-                <a target="blank" href="https://ucf.in.ua">
+                <a target="_blank" href="https://ucf.in.ua">
                     <img src={`${process.env.PUBLIC_URL}/static/images/footer/sponsors/ucf.svg`}
                          className="logo" alt="ucf"/>
                 </a>
 
-                <a target="blank" href="https://prostirsvobody.org">
+                <a target="_blank" href="https://prostirsvobody.org">
                     <img src={`${process.env.PUBLIC_URL}/static/images/footer/sponsors/prostir.jpg`}
                          className="logo" alt="prostir_svobodi"/>
                 </a>
