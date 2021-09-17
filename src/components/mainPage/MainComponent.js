8e5aba5632d80937a37c1d81cc856423a9f2f77b
RostyslavNovoselskyi <rostuk2713@ukr.net>
2021-09-17 14:57:39 +0300

    added banner to mainPage

--- src/components/mainPage/MainComponent.js
+++ src/components/mainPage/MainComponent.js
@@ -2,12 +2,12 @@ import React from "react";
 import AboutHeader from "./MainHeader";
 import MainCarousel from "./MainCarousel";
 import {Layout} from "antd";
-import MainDescription from "./MainDescription";
 import MainChallenge from "./MainChallenge";
 
 import {items} from "./carousel/CarouselItems";
 import MainCategories from "./MainCategories";
-import AboutNews from "./MainNews";
+import MainBanner from "./MainBanner";
+
 const MainComponent = () => {
     return (
         <Layout className="global-padding">
@@ -31,6 +31,9 @@ const MainComponent = () => {
                 які вдосконалять ваші знання та навички викладати українською.'
                 imageUrl={`${process.env.PUBLIC_URL}/static/images/about/challenge_2.png`}
                 buttonLabel="Дізнатись більше"
+            />
+            <MainBanner
+                imageUrl={`${process.env.PUBLIC_URL}/static/images/about/banner.png`}
             />
         </Layout>
     );
