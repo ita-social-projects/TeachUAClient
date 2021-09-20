4ac8eedd8515c0a6d1dd0892718aa2b92e14bdc3
RostyslavNovoselskyi <rostuk2713@ukr.net>
2021-09-20 15:41:56 +0300

    fixed search problem with deleting input

--- src/components/header/HeaderComponent.js
+++ src/components/header/HeaderComponent.js
@@ -8,24 +8,19 @@ import {useLocation} from "react-router";
 
 const HeaderComponent = () => {
     const location = useLocation();
-    const page = window.location.pathname !== process.env.PUBLIC_URL && localStorage.getItem("head-component-page");
 
     searchParameters.isAdvancedSearch =
         (searchParameters.isAdvancedSearch && location.pathname === '/clubs') && true;
 
-    const onLogoClick = ()=> {
-        localStorage.setItem("head-component-page", null);
-    }
-
     return (
         <Header className="header">
             <div className="left-side-menu">
-                <Link to="/" onClick={onLogoClick}>
+                <Link to="/">
                     <div className="logo"/>
                 </Link>
             </div>
 
-            <NavMenu page={page}/>
+            <NavMenu/>
             <HeaderRight/>
         </Header>
     );
