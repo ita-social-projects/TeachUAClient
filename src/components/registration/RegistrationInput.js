c28a5a4473583c6f6b882d740fc1115c804c05ee
AndriyKolodiy <49126815+AndriyKolodiy@users.noreply.github.com>
2021-09-30 14:56:02 +0300

    Revert "Some ui bugs fix"

--- src/components/registration/RegistrationInput.js
+++ src/components/registration/RegistrationInput.js
@@ -28,18 +28,16 @@ const RegistrationInput = () => {
             registerForm.phone = e.target.value
         if (e.target.id === "email")
             registerForm.email = e.target.value
-        if (e.target.id === "password"){
-            registerForm.password = e.target.value;
-            registerForm.validateFields();
-        }
+        if (e.target.id === "password")
+            registerForm.password = e.target.value
         if (e.target.id === "confirm")
             registerForm.confirm = e.target.value
 
-        // if(registerForm.password === registerForm.confirm){
-        //     setValid(true)
-        // }else{
-        //     setValid(false)
-        // }
+        if(registerForm.password === registerForm.confirm){
+            setValid(true)
+        }else{
+            setValid(false)
+        }
 
         if (registerForm.lastName.length > 1
             && registerForm.firstName.length > 1
@@ -212,9 +210,10 @@ const RegistrationInput = () => {
                                            if (!value || getFieldValue('password') === value) {
                                                return Promise.resolve();
                                            }
-                                           // if (isValid) {
-                                           //     return Promise.resolve();
-                                           // }
+                                           if (!isValid) {
+                                               return Promise.reject(new Error('Значення поля ‘Підтвердити пароль’ має бути еквівалентним значенню поля ‘Пароль’'));
+                                           }
+                                           return Promise.resolve();
                                            return Promise.reject(new Error('Значення поля ‘Підтвердити пароль’ має бути еквівалентним значенню поля ‘Пароль’'));
                                        },
                                    }),
@@ -238,6 +237,6 @@ const RegistrationInput = () => {
 
                 </Form.Item>
         </div>
-    )
-};
-export default RegistrationInput;
+                )
+                };
+                export default RegistrationInput;
