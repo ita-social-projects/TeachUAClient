dc2185ca0820a0573e1cf887c3c78cf02f9f6932
RostFromLv <meta-x-one@meta.ua>
2021-11-14 23:52:45 +0200

    For Success LocationReview

--- src/components/addCenter/AddCenter.js
+++ src/components/addCenter/AddCenter.js
@@ -33,22 +33,17 @@ const AddCenter = ({isShowing, setShowing}) => {
         getAllClubsByUserId(getUserId()).then(response => {
             console.log(response);
             setClubs(response)
-            response.map(club => (
-                club.locations.map(location => {
-                    console.log(location)
-                    locations.push({
-                        id: location.id,
-                        name: location.name,
-                        cityName: location.city.name,
-                        districtName: location.district?.name,
-                        stationName: location.station?.name,
-                        address: location.address,
-                        latitude: location.latitude,
-                        longitude: location.longitude,
-                        phone: location.phone
-                    });
-                })
-            ))
+            locations.push({
+                id: location.id,
+                name: location.name,
+                cityName: location.city?.name,
+                districtName: location.district?.name,
+                stationName: location.station?.name,
+                address: location.address,
+                latitude: location?.latitude,
+                longitude: location?.longitude,
+                phone: location.phone
+            });
         });
         getAllCities().then(response => setCities(response));
         getAllContacts().then(response => setContacts(response));
