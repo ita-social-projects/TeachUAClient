58d3e082adba4d29302bb0c03ffe69f330ec9305
OH72 <gemboleg@gmail.com>
2021-12-03 10:52:29 +0200

    MainCarousel

--- src/components/mainPage/MainCarousel.js
+++ src/components/mainPage/MainCarousel.js
@@ -8,7 +8,7 @@ import {getAllBannerItems} from "../../service/BannerItemService";
 import {BASE_URL} from "../../service/config/ApiConfig";
 import {Link} from "react-router-dom";
 
-const MainCarousel = () => {
+const MainCarousel = (items) => {
     const carousel = useRef(null);
 
     const [bannerItems, setBannerItems] = useState([]);
@@ -20,24 +20,34 @@ const MainCarousel = () => {
     };
 
     useEffect(() => {
-        getData()
+        // getData()
     }, []);
 
     return (bannerItems ? (
             <div className="about-carousel-block">
                 <ArrowLeftOutlined className="arrow" onClick={() => carousel.current.prev()}/>
+                {/*<Carousel ref={node => carousel.current = node} className="about-carousel" autoplay>*/}
+                {/*    {bannerItems.map((item) =>*/}
+                {/*        <div>*/}
+                {/*            <div className="carousel-item" style={{*/}
+                {/*                background: `linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)), url(${BASE_URL + item.picture}) no-repeat 50% 28% / cover`*/}
+                {/*            }}>*/}
+                {/*                <div className="carousel-item-1">*/}
+                {/*                    <h2 className="label">{item.title}</h2>*/}
+                {/*                    <span className="description">{item.subtitle}</span>*/}
+                {/*                    <Link to={item.link ? item.link : "#"}><Button*/}
+                {/*                        className="details-button">Детальніше</Button></Link>*/}
+                {/*                </div>*/}
+                {/*            </div>*/}
+                {/*        </div>)}*/}
+                {/*</Carousel>*/}
                 <Carousel ref={node => carousel.current = node} className="about-carousel" autoplay>
-                    {bannerItems.map((item) =>
+                    {items.items.map((item) =>
                         <div>
                             <div className="carousel-item" style={{
-                                background: `linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)), url(${BASE_URL + item.picture}) no-repeat 50% 28% / cover`
+                                background: `linear-gradient(rgba(0, 0, 0, 0.527),rgba(0, 0, 0, 0.5)), url(${item.imageURL}) no-repeat 50% 28% / cover`
                             }}>
-                                <div className="carousel-item-1">
-                                    <h2 className="label">{item.title}</h2>
-                                    <span className="description">{item.subtitle}</span>
-                                    <Link to={item.link ? item.link : "#"}><Button
-                                        className="details-button">Детальніше</Button></Link>
-                                </div>
+                                {item.body}
                             </div>
                         </div>)}
                 </Carousel>
