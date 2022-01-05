ad3f2a2aee6de358b46c94707308b301a53063a8
OH72 <gemboleg@gmail.com>
2022-01-05 13:45:31 +0200

    Delete comment log

--- src/components/userPage/useredit/UserEditModal.js
+++ src/components/userPage/useredit/UserEditModal.js
@@ -12,7 +12,6 @@ const UserEditModal = ({user}) => {
     const [visible, setVisible] = useState(false);
 
     const onFinish = (values) => {
-        console.log("onFinish()")
         if(values.urlLogo != null && typeof values.urlLogo !== "undefined" && typeof values.urlLogo !== "string") {
             values.urlLogo = values.urlLogo.file.response
         }
