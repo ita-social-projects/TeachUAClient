f116c3aa2850f579ebd45a0f34dae0e1ef2e51b7
TarasLyzunyk <mr.lyzunyk@gmail.com>
2022-02-02 15:29:50 +0200

    Add button to Search for download result output as pdf

--- src/service/SearchService.js
+++ src/service/SearchService.js
@@ -1,6 +1,7 @@
 import fetchRequest from "./FetchRequest";
-import { BASE_URL } from "./config/ApiConfig";
+import {BASE_URL} from "./config/ApiConfig";
 import {searchInputData, searchParameters} from "../context/SearchContext";
+import {handleDownloadFile} from "../util/FileUtil";
 
 export const getPossibleResults = async (parameters) => {
     return await fetchRequest.get(BASE_URL + "/api/search", {
@@ -23,3 +24,19 @@ export const getPossibleResultsByText = async (text, parameters) => {
         return response.data
     });
 };
+
+export const getResultSearchReport = async (parameters, fileName) => {
+    const response = await fetchRequest.get(BASE_URL + `/api/pdf/resultsearch`,
+        {
+            method: "get",
+            responseType: "blob",
+            params: {
+                clubName: searchInputData.input,
+                cityName: parameters.cityName,
+                isOnline: parameters.isOnline,
+                categoryName: parameters.categoryName
+            }
+        }
+    )
+    handleDownloadFile(response.data, fileName, "pdf")
+}
