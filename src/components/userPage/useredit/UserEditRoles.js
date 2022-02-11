472e04fffabcaa3559103d87ce7bc16bc0e74ee0
TarasLyzunyk <mr.lyzunyk@gmail.com>
2022-02-11 14:01:45 +0200

    Make buttons disable, for User and Admin

--- src/components/userPage/useredit/UserEditRoles.js
+++ src/components/userPage/useredit/UserEditRoles.js
@@ -2,27 +2,29 @@ import React from 'react';
 import UserOutlined from "@ant-design/icons/lib/icons/UserOutlined";
 import {Form, Radio} from "antd";
 
-const UserEditRoles =({user}) => {
-function disable(value){
-    if (value===user.roleName){
-        return false;
+const UserEditRoles = ({user}) => {
+    function disable(value) {
+        if (value === "ROLE_USER" || value ==="ROLE_ADMIN") {
+            return true;
+        } else if (value === "ROLE_MANAGER")
+            return false;
     }
-    else return true;
-}
+
     return (
-        <Form.Item name="roleName"  initialValue={user.roleName}>
+        <Form.Item name="roleName" initialValue={user.roleName}>
             <Radio.Group className="button-container"
                          optionType="button"
                          buttonStyle="solid"
             >
-
-                <Radio.Button value="ROLE_USER">
+                <Radio.Button value="ROLE_USER"
+                              disabled={disable(user.roleName)}>
                     <div className="button-box">
                         <div className="ellipse"><UserOutlined className="user-icon"/></div>
                         <div className="role-name"> Відвідувач</div>
                     </div>
                 </Radio.Button>
-                <Radio.Button value="ROLE_MANAGER">
+                <Radio.Button value="ROLE_MANAGER"
+                              disabled={disable(user.roleName)}>
                     <div className="button-box">
                         <div className="ellipse"><UserOutlined className="user-icon"/></div>
                         <div className="role-name"> Керівник</div>
