9509007760f847a09717fa5795d56cfd0305ea39
Roman Bodnar <kylt1983@gmail.com>
2022-06-04 00:13:09 +0300

    add coordinates ("lon., lat.") to location and 0-phone if null to be able to create new center

--- src/components/addCenter/MainInformation.js
+++ src/components/addCenter/MainInformation.js
@@ -25,6 +25,14 @@ const MainInformation = ({ step, setStep, clubs, cities, locations, setLocations
     const onFinish = (values) => {
         console.log('Success: ', values);
         setResult(Object.assign(result, values));
+        result.locations = [JSON.parse(result.locations)];
+        result.locations.map(
+            location => {
+                location.coordinates = location.coordinates || location.latitude + ", " + location.longitude;
+                location.phone = location.phone || "0000000000";
+            }
+        );
+        console.log(result.locations);
         mainInformationFrom.resetFields();
         nextStep();
     };
@@ -69,7 +77,7 @@ const MainInformation = ({ step, setStep, clubs, cities, locations, setLocations
                         <Checkbox.Group>
                             {locations.map(location =>
                                 <div className="checkbox-item">
-                                    <Checkbox value={location}>
+                                    <Checkbox value={JSON.stringify(location)}>
                                         {location.name}
                                     </Checkbox>
                                 </div>
