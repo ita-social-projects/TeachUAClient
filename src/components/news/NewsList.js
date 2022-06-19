0d71e07729983ffcd9411e4d7c9ffe262b017f63
bakaydmytro <bakay.dmytro@gmail.com>
2022-06-19 21:45:09 +0300

    News page minor bugs fix

--- src/components/news/NewsList.js
+++ src/components/news/NewsList.js
@@ -1,36 +1,17 @@
-import React, {useEffect, useState} from "react";
-import {getNewsList} from "../../service/NewsService";
+import React from "react";
 import NewsItem from "./NewsItem";
 import EmptySearch from "../EmptySearch";
 import {Pagination} from "antd";
 import "./css/NewsList.css";
-import Loader from "../Loader";
 
-const NewsList = ({load,setLoad}) => {
-    const [currentPage, setCurrentPage] = useState(0);
-
-    const [newsList, setNewsList] = useState([]);
-
-    useEffect(() => {
-        getData(currentPage);
-    }, []);
-
-    const getData = (page) =>{
-        getNewsList(page).then(response => {
-            setLoad(true);
-            console.log(response);
-            setNewsList(response);
-            setLoad(false);
-        })
-    }
+const NewsList = ({newsList,currentPage, setCurrentPage}) => {
 
     const onPageChange = (page) => {
         setCurrentPage(page - 1);
-        getData(page - 1);
         window.scrollTo(0, 0);
     };
 
-    return load ? <Loader/> : newsList.length === 0 ? <EmptySearch/> : (
+    return newsList.length === 0 ? <EmptySearch/> : (
         <div>
             {newsList.content.map((news, index) => <NewsItem key={index} news={news}/>)}
             <Pagination className="pagination"
