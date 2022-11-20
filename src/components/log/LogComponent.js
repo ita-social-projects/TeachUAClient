05fb7732f7ac6aef480ee6e4ee5d16d71fc9f23d
Yevhen Danylov <dvavoly@outlook.com>
2022-11-20 13:51:11 +0200

    Added search by log name and notification message for actions

--- src/components/log/LogComponent.js
+++ src/components/log/LogComponent.js
@@ -1,53 +1,107 @@
-import React, { useEffect, useState } from "react";
-import { getAllLogs, getLogByName, deleteLogByName } from "../../service/LogService";
-import { Button, Divider, List, Modal, Space, Table } from "antd";
-import "./css/LogComponent.css"
+import React, { useEffect, useState } from 'react'
+import {
+    getAllLogs,
+    getLogByName,
+    deleteLogByName,
+    downloadLogByName,
+} from '../../service/LogService'
+import './css/LogComponent.css'
+import {
+    Button,
+    Divider,
+    Input,
+    List,
+    message,
+    Modal,
+    Space,
+    Table,
+} from 'antd'
 
 const LogComponent = () => {
+    const [log, setLog] = useState([])
+    const [logs, setLogs] = useState([])
+    const [searchQuery, setSearchQuery] = useState('teachualogs-')
+    const [isModalOpen, setIsModalOpen] = useState(false)
 
-    const [logs, setLogs] = useState([]);
-    const [log, setLog] = useState([]);
-    const [isModalOpen, setIsModalOpen] = useState(false);
+    const { Search } = Input
 
     const getData = () => {
-        getAllLogs().then(response => setLogs(response.data));
-    };
-
-    const deleteLog = (fileName) => {
-        deleteLogByName(fileName).then(() => getData());
+        getAllLogs().then((response) => setLogs(response.data))
     }
 
-    const showModal = (fileName) => {
-        setIsModalOpen(true);
-        getLogByName(fileName).then(response => setLog(response.data));
-    };
+    const success = (fileName) => {
+        message.success(`File ${fileName} removed successfully`)
+    }
+
+    const error = (fileName) => {
+        message.error(`Cannot delete file: ${fileName}`)
+    }
+
+    const deleteLog = ({ fileName }) => {
+        deleteLogByName(fileName)
+            .then(() => {
+                success(fileName)
+                getData()
+            })
+            .catch(() => {
+                error(fileName)
+                getData()
+            })
+    }
+
+    const showModal = ({ fileName }) => {
+        setIsModalOpen(true)
+        getLogByName(fileName).then((response) => setLog(response.data))
+    }
 
     const setShowing = () => {
-        setIsModalOpen(false);
-    };
+        setIsModalOpen(false)
+    }
 
-    const dataSource = logs.map((log, index) => ({ number: index + 1, fileName: log }));
+    const onSearch = (value) => setSearchQuery(value)
+
+    const dataSource = logs
+        .filter((el) => el.includes(searchQuery))
+        .map((log, index) => ({
+            number: index + 1,
+            fileName: log,
+        }))
 
     const columns = [
         {
             title: '#',
             dataIndex: 'number',
             key: 'number',
+            width: 75,
         },
         {
             title: 'File name',
             dataIndex: 'fileName',
             key: 'fileName',
+            width: 400,
         },
         {
             title: 'Action',
             key: 'action',
+            width: 200,
             render: (_, record) => (
                 <Space size="middle">
-                    <Button onClick={() => showModal(record.fileName)}>
+                    <Button
+                        className="orange-btn"
+                        onClick={() => showModal(record)}
+                    >
                         View
                     </Button>
-                    <Button onClick={() => deleteLog(record.fileName)}>
+                    <Button
+                        className="orange-btn"
+                        onClick={() => downloadLogByName(record)}
+                    >
+                        Download
+                    </Button>
+                    <Button
+                        className="orange-btn"
+                        onClick={() => deleteLog(record)}
+                    >
                         Delete
                     </Button>
                 </Space>
@@ -56,13 +110,30 @@ const LogComponent = () => {
     ]
 
     useEffect(() => {
-        getData();
+        getData()
     }, [])
 
     return (
         <>
             <Divider />
-            <Space direction="horizontal" style={{ width: '100%', justifyContent: 'center' }}>
+            <Space
+                direction="horizontal"
+                style={{ width: '100%', justifyContent: 'center' }}
+            >
+                <Search
+                    allowClear
+                    defaultValue={searchQuery}
+                    onSearch={onSearch}
+                    placeholder="input search file name"
+                    style={{
+                        width: 304,
+                    }}
+                />
+            </Space>
+            <Space
+                direction="horizontal"
+                style={{ width: '100%', justifyContent: 'center' }}
+            >
                 <Table
                     dataSource={dataSource}
                     columns={columns}
@@ -75,6 +146,7 @@ const LogComponent = () => {
                     onOk={() => setShowing(false)}
                     onCancel={() => setShowing(false)}
                     width={1100}
+                    footer={null}
                 >
                     <List
                         size="small"
@@ -84,8 +156,9 @@ const LogComponent = () => {
                     />
                 </Modal>
             </Space>
+            <Divider />
         </>
     )
-};
+}
 
-export default LogComponent;
+export default LogComponent
