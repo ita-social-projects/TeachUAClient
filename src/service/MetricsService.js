b756f5301ee49cbb69006e4e77bea98d29cd6c75
Danbug <unwantedmouth@gmail.com>
2023-02-05 17:54:21 +0200

    Reformatted request url

--- src/service/MetricsService.js
+++ src/service/MetricsService.js
@@ -2,7 +2,7 @@ import {BASE_URL} from "./config/ApiConfig";
 import fetchRequest from "./FetchRequest";
 
 export const getAllMetrics = () => {
-    return fetchRequest.get(BASE_URL + "/api/prometheus1")
+    return fetchRequest.get(BASE_URL + "/api/prometheusKeysAndValues")
         .then((response) => {
             return response.data;
         }).catch((error) => {
