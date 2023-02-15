3e2f8e3271c4aee428c6bc9e7e9506ea23e25d2e
BNazar002 <bilyk.nazar00@gmail.com>
2023-02-15 20:47:58 +0200

    Changed color palette

--- craco.config.js
+++ craco.config.js
@@ -12,7 +12,8 @@ module.exports = {
                             '@button-color': '#FA8C16',
                             '@link-color': '#000',
                             '@text-color': '#000',
-                            '@primary-hover-color': '#FA8C16'
+                            '@primary-hover-color': '#FA8C16',
+                            '@menu-highlight-color': '#FFF'
                         },
                         javascriptEnabled: true,
                     },
