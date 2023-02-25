8e2b1eb605f0c59c4f22f5a25cb2a249a6260e58
BNazar002 <bilyk.nazar00@gmail.com>
2023-02-25 13:53:37 +0200

    Fixed center list item displaying

--- src/components/centerList/CenterListRectangleItem.js
+++ src/components/centerList/CenterListRectangleItem.js
@@ -1,24 +1,20 @@
-import { Button, Card } from "antd";
+import {Button, Card, Popover, Rate} from "antd";
 import EnvironmentFilled from "@ant-design/icons/lib/icons/EnvironmentFilled";
 import {Link} from "react-router-dom";
 import PropTypes from "prop-types";
-import React from "react";
+import React, {useState} from "react";
 import CenterLogo from "./CenterLogo";
 import "./css/CenterListRectangleItem.css";
-import { Rate } from "antd";
-import {   Popover} from "antd";
-import  { useState } from "react";
-import EyeOutlined from "@ant-design/icons/lib/icons/EyeOutlined";
 
 
 const CenterListRectangleItem = ({center, onCenterClick}) => {
     const [visible, setVisible] = useState(false);
 
-    const getRating= () =>{
+    const getRating = () => {
         let rating = 0;
         let ratedClubs = 0;
         center.clubs.forEach(club => {
-            if(parseInt(club.rating)){
+            if (parseInt(club.rating)) {
                 rating += parseInt(club.rating);
                 ratedClubs++;
             }
@@ -35,38 +31,43 @@ const CenterListRectangleItem = ({center, onCenterClick}) => {
                         <div className="center-name">{center.name}</div>
                     </div>
                 </div>
-              
+
+                <div className="item-rectangle-row">
                     <div className="item-rating-address">
-                    <Rate className="rating" disabled value={getRating()}/>
-                          {
-                    center.locations.length > 0 &&
-                    <div className="center-address" onClick={() => { setVisible(true) }} >
-                        <EnvironmentFilled
-                            className="address-icon" />
+                        <Rate className="rating" disabled value={getRating()}/>
                         {
-                            center.locations.length === 1 ? <span className="oneAddress"> {center.locations[0].address}</span>
-                                :
-                                <Popover
-                                    className="popover"
-                                    title="Локації"
-                                    placement="topRight"
-                                    content={center.locations.map(location =>
-                                        <div>
-                                            <EnvironmentFilled className="address-small-icon" />
-                                            <span className="text"> {location.address}</span>
-                                        </div>
-                                    )}>
-                                    <span className="text"><span className="oneAddress"
-                                    >{center.locations[0].address}</span>{center.locations.length-1}</span>
-                                    <EyeOutlined className="expand-icon" />
-                                </Popover>
+                            center.locations.length > 0 &&
+                            <div className="center-address" onClick={() => {
+                                setVisible(true)
+                            }}>
+                                <EnvironmentFilled
+                                    className="address-icon"/>
+                                {
+                                    center.locations.length === 1 ?
+                                        <span className="oneAddress"> {center.locations[0].address}</span>
+                                        :
+                                        <Popover
+                                            className="popover"
+                                            title="Локації"
+                                            placement="topRight"
+                                            content={center.locations.map(location =>
+                                                <div>
+                                                    <EnvironmentFilled className="address-small-icon"/>
+                                                    <span className="text"> {location.address}</span>
+                                                </div>
+                                            )}>
+                                    <span className="text" style={{display: "flex", alignItems: "center"}}><span
+                                        className="oneAddress"
+                                    >{center.locations[0].address}</span>, і ще {center.locations.length - 1}</span>
+                                        </Popover>
+                                }
+                            </div>
                         }
                     </div>
-                }
                     <Button className="outlined-button details-button">
                         <Link to={`/center/${center.id}`}>Детальніше</Link>
                     </Button>
-                    </div>
+                </div>
             </Card>
         </div>
     )
