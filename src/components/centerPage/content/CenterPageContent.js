14b8ef42f7a4d5be308316cfd82b21a2c199f00b
NadiiaNanivska <nanivskan14@gmail.com>
2023-07-03 13:10:04 +0300

    Added rate of center

--- src/components/centerPage/content/CenterPageContent.js
+++ src/components/centerPage/content/CenterPageContent.js
@@ -7,28 +7,13 @@ import {convertToHtml} from "../../editor/EditorConverter";
 import {getClubReport} from "../../../service/ClubService";
 import {FilePdfOutlined} from "@ant-design/icons";
 import {getCenterReport, getAllCenterClubsByCenterId} from "../../../service/CenterService";
-import { centerFeedback } from "../../../util/CenterUtil";
 import PageRatingCenter from "./PageRatingCenter";
 
 const CenterPageContent = ({ center, loading }) => {
 
-    const [rate, setRate] = useState(0);
-    const [feedbackCount, setFeedbackCount] = useState(0);
-
-    const feedback = getAllCenterClubsByCenterId(center.id);
-
-    feedback.then((value) => {
-        var count = 0;
-        for (let i = 0; i < value.length; i++) {
-            count += value[i].feedbackCount;
-        }
-        setRate(centerFeedback(value));
-        setFeedbackCount(count);
-    })
-
     return (
         <Content className="page-content">
-            <PageRatingCenter rating={rate} count={feedbackCount}/>
+            <PageRatingCenter rating={center.rating} count={center.feedbackCount}/>
             {!center ?
                 <div className="content">У цього центру опису немає...</div>
                 :
