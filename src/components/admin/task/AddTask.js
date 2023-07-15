449ed136f0d59b9ab1e511db5825e9972089ab3b
Vitalik2705 <vitok2misze@gmail.com>
2023-07-15 18:53:32 +0300

    The dropdown calendar for selecting the day of starting the current task localized on 'Додайте завдання' page

--- src/components/admin/task/AddTask.js
+++ src/components/admin/task/AddTask.js
@@ -9,6 +9,8 @@ import {getAllChallenges} from "../../../service/ChallengeService";
 import {Link, useHistory } from "react-router-dom";
 import ChallengesInTasks from "./ChallengesInTasks";
 import {tokenToHeader} from "../../../service/UploadService";
+import locale from 'antd/es/date-picker/locale/uk_UA';
+
 const { Option } = Select;
 
 const { Title } = Typography;
@@ -124,6 +126,7 @@ const AddTask = () => {
                             onChange={onDateChange}
                             format={dateFormat}
                             value={startDate}
+                            locale={locale}
                         />
                 </Form.Item>
                 <Form.Item
