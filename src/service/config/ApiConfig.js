046ce01cfcc5992e473fc5e6030e91c416e86d74
Jaroslav <garasym@gmail.com>
2024-01-04 20:38:52 +0200

    ApiConfig.js updated

--- src/service/config/ApiConfig.js
+++ src/service/config/ApiConfig.js
@@ -5,5 +5,5 @@ export const DOWNLOAD_DATABASE_SQL = BASE_URL + "/api/download-database-sql";
 export const DELETE_FILE_URL = BASE_URL + "/api/delete-file";
 export const SERVICE_EMAIL = "fcrnx-hxenvavna@fcrnx-hxenvavna-362008.vnz.tfreivprnppbhag.pbz";
 
-export const FRONTEND_COMMIT_NUMBER="d5b9eeadf6f7e7280cb4c802e1dab64232e75e98";
+export const FRONTEND_COMMIT_NUMBER="fff03237d06f7b2d130e4c190953bf51d6b47f0a";
 export const FRONTEND_COMMIT_DATE="04.01.2024";
